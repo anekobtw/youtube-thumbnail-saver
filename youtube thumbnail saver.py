e7b30826950e8db016bb189cb956de934992620e
ssayand <krolexonder@gmail.com>
2023-06-03 11:39:02 +0400

    v1.0.2

--- youtube thumbnail saver.py
+++ youtube thumbnail saver.py
@@ -1,35 +1,38 @@
+from colorama import Fore, init
 import requests
-import pytube
-VERSION = '1.0.1'
+import os
 
-print(f"""
+init(autoreset=True)
+VERSION = '1.0.2'
+
+def sanitize_title(title):
+    invalid_chars = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
+    return ''.join(char for char in title if char not in invalid_chars)
+
+
+while True:
+    print(f"""
 --------------------------------
 YouTube Thumbnail Saver v{VERSION}.
 © Aneko, 2023
 --------------------------------
 """)
-
-def sanitize_title(title):
-    invalid_chars = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
-    for char in invalid_chars:
-        title = title.replace(char, '')
-    return title
-
-while True:
+    
     link = input('Enter video link: ')
     
     # Getting a title
     response = requests.get(f'https://noembed.com/embed?url={link}')
-    video_title = sanitize_title(str(response.json()['title']))
+    video_title = sanitize_title(response.json()['title'])
     
     # Getting a thumbnail
-    thumbnail_url = pytube.YouTube(link).thumbnail_url
+    thumbnail_url = response.json()['thumbnail_url']
 
     # Saving an image
     response = requests.get(thumbnail_url)
     with open(f"{video_title}.jpg", "wb") as f:
         f.write(response.content)
-        print(f'Saved as {video_title}.jpg\n')
+        print(f'Saved as {Fore.GREEN}{video_title}.jpg\n')
 
     if input('Do you want to download another thumbnail? (Y/n) ').lower() not in ['yes', 'y']:
         break
+    os.system('cls')
