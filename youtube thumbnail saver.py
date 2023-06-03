b3cdb21c1fa1c88064a53bd838e76c879c0ae0ec
ssayand <krolexonder@gmail.com>
2023-06-03 17:27:26 +0400

    v1.1

--- youtube thumbnail saver.py
+++ youtube thumbnail saver.py
@@ -1,13 +1,28 @@
 from colorama import Fore, init
 import requests
+import pytube
+import time
 import os
 
 init(autoreset=True)
-VERSION = '1.0.2'
+VERSION = '1.1'
 
-def sanitize_title(title):
+def save_thumbnail(link):
+    response = requests.get(f'https://noembed.com/embed?url={link}')
+    
+    # Getting a title
+    video_title = response.json()['title']
     invalid_chars = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
-    return ''.join(char for char in title if char not in invalid_chars)
+    video_title = ''.join(char for char in video_title if char not in invalid_chars)
+
+    # Getting a thumbnail
+    thumbnail_url = response.json()['thumbnail_url']
+
+    # Saving an image
+    with open(f'{video_title}.png', "wb") as f:
+        response = requests.get(thumbnail_url)
+        f.write(response.content)
+        print(f'Saved as {Fore.GREEN}{video_title}.png\n')
 
 
 while True:
@@ -17,21 +32,26 @@ YouTube Thumbnail Saver v{VERSION}.
 © Aneko, 2023
 --------------------------------
 """)
-    
-    link = input('Enter video link: ')
-    
-    # Getting a title
-    response = requests.get(f'https://noembed.com/embed?url={link}')
-    video_title = sanitize_title(response.json()['title'])
-    
-    # Getting a thumbnail
-    thumbnail_url = response.json()['thumbnail_url']
 
-    # Saving an image
-    response = requests.get(thumbnail_url)
-    with open(f"{video_title}.jpg", "wb") as f:
-        f.write(response.content)
-        print(f'Saved as {Fore.GREEN}{video_title}.jpg\n')
+    download_type = input('''What do you want to download?
+[1] Video thumbnail.
+[2] All the thumbnails from the playlist.
+''')
+
+    if download_type == '1':
+        link = input('Enter the video link: ')
+        save_thumbnail(link)
+
+    elif download_type == '2':
+        link = input('Enter the link to the playlist: ')
+        playlist = pytube.Playlist(link).videos
+        for video in playlist:
+            save_thumbnail(video.watch_url)
+
+    else:
+        print(f'{Fore.RED}Error occured.')
+        time.sleep(1)
+        quit()
 
     if input('Do you want to download another thumbnail? (Y/n) ').lower() not in ['yes', 'y']:
         break
